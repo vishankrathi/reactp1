e9871dc5fc90720fd29a37ac32da4fa26df626fd
Vishank <vishankrathi724@gmail.com>
2023-12-03 01:02:38 -0800

    first commit

---
+++ src/components/Bharat.js
@@ -0,0 +1,33 @@
+const Bharat=()=>{
+    return(
+        <main className="hero container">
+        <div className="hero-content">
+            <h1>YOUR FEET  DESERVE BEST</h1>
+            <p>YOUR FEET DESERVE THE BEST AND WERE HERE TO HELP YOU WITH OUR 
+                SHOES.YOUR FEET DESERVE THE BEST AND WERE AND HERE TO HELP
+                TO HELP YOU WITH
+                OUR 
+                SHOES.
+              </p>
+
+        <div className="hero-btn">
+            <button>shop now</button>
+            <button className="secondary-btn">category</button>
+
+        </div>
+        <div className="shopping">
+            <p>also Available on</p>
+            <div className="brand-icons">
+                <img src="/images/amazon.png" alt="" />
+                <img src="/images/flipkart.png" alt="" />
+            </div>
+        </div>
+    </div>
+            <div className="hero-image">
+                <img src="/images/hero-image.png" alt="hero-image"></img>
+            </div>|
+        </main>
+    );
+    };
+
+export default Bharat;
---
+++ src/components/Manu.js
@@ -0,0 +1,19 @@
+const  Manu =()=>{
+    return(
+     <nav class="container">
+      <div >
+        <img  src="/images/brand_logo (1).png" alt="logo"></img>
+        </div>
+        <ul>
+          <li href="#">menu</li>
+          <li href="#">about</li>
+          <li href="#">location</li>
+          <li href="#">contact</li>
+        </ul>
+       <button>login</button>
+     </nav>
+
+  );
+};
+
+export default Manu;
